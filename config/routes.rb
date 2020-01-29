2e0d251769c8ff70dc86dc6ef068c47765af3200
EC2 Default User <ec2-user@ip-172-31-24-211.us-east-2.compute.internal>
2020-01-29 08:34:58 +0000

    dd

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+  get'users/:id' ,to:"users#show"
+  get 'signup' ,to:'users#new'
+  root to:"homes#index"
+  get'/memos/index',to:"memos#index"
+  get '/new',to:"memos#new"
+  post '/create' ,to:"memos#create"
+  delete '/memos/:id' ,to:'memos#destroy'
+  get '/memos/:id/edit' ,to:'memos#edit'
+  patch '/memos/:id' ,to:'memos#update'
+  get '/categories/:id',to:'categories#show'
+  resources :users
+end
